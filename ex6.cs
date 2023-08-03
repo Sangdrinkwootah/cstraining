d08c2f7e8f6de86835aa25af38f16876b3cd97a5
Sang Nguyen Huu <86295586+Sangdrinkwootah@users.noreply.github.com>
2023-08-03 10:56:38 +0700

    Update ex6.cs

--- ex6.cs
+++ ex6.cs
@@ -3,6 +3,12 @@ class HocSinh
     public string Name { get; set; }
     public int Age { get; set; }
     public string Location { get; set; }
+    public HocSinh(string name, int age, string location)
+    {
+        Name = name;
+        Age = age;
+        Location = location;
+    }
 }
 
 class QLHS
@@ -24,7 +30,7 @@ class QLHS
         {
             if (h.Age == 20)
             {
-                System.Console.WriteLine("Name: " + h.Name + "/nAge: 20/nQue quan: " + h.Location);
+                System.Console.WriteLine("Name: " + h.Name + "\nAge: 20\nQue quan: " + h.Location);
             }
         }
     }
@@ -35,7 +41,7 @@ class QLHS
         {
             if (h.Age == 23 && h.Location == "DN")
             {
-                System.Console.WriteLine("Name: " + h.Name + "/nAge: 20/nQue quan: " + h.Location);
+                System.Console.WriteLine("Name: " + h.Name + "\nAge: 20\nQue quan: " + h.Location);
             }
         }
     }
@@ -46,6 +52,13 @@ class GFG
     static void Main(string[] args)
     {
         QLHS dshs = new QLHS();
+        HocSinh hs1 = new HocSinh("Nguyen Van A", 20, "X");
+        HocSinh hs2 = new HocSinh("Nguyen Van B", 23, "Y");
+        HocSinh hs3 = new HocSinh("Tran Thi C", 23, "DN");
+        dshs.ThemHocSinh(hs1);
+        dshs.ThemHocSinh(hs2);
+        dshs.ThemHocSinh(hs3);
+
         dshs.HienThiHocSinh20t();
         dshs.HienThiHocSinh23t();
     }
